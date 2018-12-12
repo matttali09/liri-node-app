ce510cac8aadca42ef079729d24939ff6686b79d
matttali09 <matttaliancich09@gmail.com>
2018-12-12 11:52:29 -0500

    this

--- bandsintown.js
+++ bandsintown.js
@@ -11,16 +11,16 @@ var queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app
 // This line is just to help us debug against the actual URL.
 
 axios.get(queryUrl).then(
-  function(response) {
-// console.log(response);
+  function (response) {
+    // console.log(response);
 
     for (i = 0; i < response.data.length; i++) {
-        console.log("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
-        console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
-        console.log(`Venue Name: ${response.data[i].venue.name}`)
-        console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}`);
-        console.log("Date Of Concert: " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
-        console.log("|______________________________________|")
-        console.log("");      
+      console.log("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
+      console.log(`Artist Lineup: ${response.data[i].lineup.join(", ")}`)
+      console.log(`Venue Name: ${response.data[i].venue.name}`)
+      console.log(`Location: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}`);
+      console.log("Date Of Concert: " + moment(response.data[i].datetime).format("MM/DD/YYYY"))
+      console.log("|______________________________________|")
+      console.log("");
     }
   });
